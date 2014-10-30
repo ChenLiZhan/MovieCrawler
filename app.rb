d1a3574861f1de7113f1c2de8a2bc7266ea94e2f
transedward <transedward@gmail.com>
2014-10-30 17:05:49 +0800

    init app.rb and fix some rubocop

--- app.rb
+++ app.rb
@@ -2,6 +2,41 @@ require 'sinatra/base'
 require 'movie_crawler'
 require 'json'
 
+# web version of MovieCrawlerApp(https://github.com/ChenLiZhan/SOA-Crawler)
 class MovieCrawlerApp < Sinatra::Base
+  helpers do
+    def get_ranks(category)
+      ranks_after = {
+        'type' => 'rank_table',
+        'category' => category,
+        'rank' => []
+      }
 
+      category = params[:category]
+      ranks_after['rank'] = MovieCrawler::MovieInfo.get_table(category)
+      ranks_after
+    end
+
+    def get_infos(category)
+      infos_after = {
+        'type' => 'info_list',
+        'category' => category,
+        'info' => []
+      }
+
+      category = params[:category]
+      infos_after['info'] = MovieCrawler::MovieInfo.movies_parser(category)
+      infos_after
+    end
+  end
+
+  get '/api/v1/moviecrawler/rank/:category.json' do
+    content_type :json
+    get_ranks(params[:category]).to_json
+  end
+
+  get '/api/v1/moviecrawler/info/:category.json' do
+    content_type :json
+    get_infos(params[:category]).to_json
+  end
 end
