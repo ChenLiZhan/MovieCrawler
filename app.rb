1fc39f21bf6e6499455ab3fc75d2c4eab4e8d356
transedward <transedward@gmail.com>
2014-11-02 23:07:20 +0800

    add RANK_LIST

--- app.rb
+++ app.rb
@@ -4,11 +4,13 @@ require 'json'
 
 # web version of MovieCrawlerApp(https://github.com/ChenLiZhan/SOA-Crawler)
 class MovieCrawlerApp < Sinatra::Base
+  RANK_LIST = { '1' => 'Latest', '2' => 'Second_round' }
+
   helpers do
     def get_ranks(category)
       ranks_after = {
         'type' => 'rank_table',
-        'category' => category,
+        'category' => RANK_LIST[category],
         'rank' => []
       }
 
@@ -30,12 +32,12 @@ class MovieCrawlerApp < Sinatra::Base
     end
   end
 
-  get '/api/v1/moviecrawler/rank/:category.json' do
+  get '/api/v1/rank/:category.json' do
     content_type :json
     get_ranks(params[:category]).to_json
   end
 
-  get '/api/v1/moviecrawler/info/:category.json' do
+  get '/api/v1/info/:category.json' do
     content_type :json
     get_infos(params[:category]).to_json
   end
